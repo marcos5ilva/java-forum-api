2f3b232a1130e0d8b335f0a78bc1bf4aa8fee9c8
marcos <marcos.silva79@gmail.com>
2020-12-30 09:30:47 -0500

    feat: add SecurityConfiguration class methods

--- src/main/java/com/courses/forum/config/security/SecurityConfigurations.java
+++ src/main/java/com/courses/forum/config/security/SecurityConfigurations.java
@@ -1,6 +1,10 @@
 package com.courses.forum.config.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -8,5 +12,24 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
 
+    //Authentication
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 
+    }
+
+    //Authorization
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers(HttpMethod.GET,"/topics").permitAll()
+                .antMatchers(HttpMethod.GET,"/topics/*").permitAll();
+    }
+
+
+    //Static resources (js, css, images)
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+
+    }
 }
